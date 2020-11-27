750da5fa643a8e4449714a91bd316aae8e6f3a70
dreamvibe1993 <dreamvibe@mail.ru>
2020-11-27 14:38:46 +0500

    enabled the line

--- js/login-interaction.js
+++ js/login-interaction.js
@@ -29,69 +29,69 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
-// let targid;
-// let anchorcoords;
-//
-// function prepositiontheline() {
-//   let title = document.title.toLowerCase();
-//   let home;
-//   function gogoline(v) {
-//     let homecoords = v.getBoundingClientRect();
-//     line.style.width = v.clientWidth + 'px';
-//     line.style.height = '2px';
-//     line.style.left = homecoords.left + 'px';
-//     line.style.top = homecoords.y + homecoords.height - 2 + 'px';
-//   }
-//   if (title == 'tortiko') {
-//     home = document.querySelector('#home');
-//     gogoline(home);
-//   } else if (title == 'item') {
-//     home = document.querySelector(`#shop`);
-//     gogoline(home);
-//   } else {
-//     home = document.querySelector(`#${title}`);
-//     gogoline(home);
-//   }
-//
-//
-//   targid = home.id;
-// }
-// prepositiontheline();
-//
-//
-//
-// navigationbar.addEventListener('mouseover', (e) => {
-//     if (e.target.tagName == 'LI' && e.target.innerHTML) {
-//       let tgclientrect = e.target.getBoundingClientRect();
-//       line.style.width = e.target.clientWidth + 'px';
-//       line.style.height = '2px';
-//       line.style.left = tgclientrect.left + 'px';
-//       line.style.top = tgclientrect.y + tgclientrect.height - 2 + 'px';
-//       targid = e.target.id;
-//     }
-//     if (e.target.tagName == 'A' && e.target.innerHTML) {
-//       let tgclientrect = e.target.parentElement.getBoundingClientRect();
-//       line.style.width = e.target.parentElement.clientWidth + 'px';
-//       line.style.height = '2px';
-//       line.style.left = tgclientrect.left + 'px';
-//       line.style.top = tgclientrect.y + tgclientrect.height - 2 + 'px';
-//       targid = e.target.parentElement.id;
-//     }
-// });
-//
-// navigationbar.addEventListener('mouseleave', (e) => {
-//   setTimeout(() => {prepositiontheline()}, 1000)
-// });
-//
-// document.addEventListener('mouseover', function(event) {
-//   anchorcoords = event.target.closest('[data-anchor]');
-// });
-//
-//
-// window.onresize = function(event) {
-//   line.style.top = navigationbar.offsetHeight - 2 + 'px';
-//   let currelem = document.querySelector(`#${targid}`);
-//   let elempos = currelem.getBoundingClientRect();
-//   line.style.left = elempos.left + 'px';
-//   anchorcoords.scrollIntoView(top);
-// };
+let targid;
+let anchorcoords;
+
+function prepositiontheline() {
+  let title = document.title.toLowerCase();
+  let home;
+  function gogoline(v) {
+    let homecoords = v.getBoundingClientRect();
+    line.style.width = v.clientWidth + 'px';
+    line.style.height = '2px';
+    line.style.left = homecoords.left + 'px';
+    line.style.top = homecoords.y + homecoords.height - 2 + 'px';
+  }
+  if (title == 'tortiko') {
+    home = document.querySelector('#home');
+    gogoline(home);
+  } else if (title == 'item') {
+    home = document.querySelector(`#shop`);
+    gogoline(home);
+  } else {
+    home = document.querySelector(`#${title}`);
+    gogoline(home);
+  }
+
+
+  targid = home.id;
+}
+prepositiontheline();
+
+
+
+navigationbar.addEventListener('mouseover', (e) => {
+    if (e.target.tagName == 'LI' && e.target.innerHTML) {
+      let tgclientrect = e.target.getBoundingClientRect();
+      line.style.width = e.target.clientWidth + 'px';
+      line.style.height = '2px';
+      line.style.left = tgclientrect.left + 'px';
+      line.style.top = tgclientrect.y + tgclientrect.height - 2 + 'px';
+      targid = e.target.id;
+    }
+    if (e.target.tagName == 'A' && e.target.innerHTML) {
+      let tgclientrect = e.target.parentElement.getBoundingClientRect();
+      line.style.width = e.target.parentElement.clientWidth + 'px';
+      line.style.height = '2px';
+      line.style.left = tgclientrect.left + 'px';
+      line.style.top = tgclientrect.y + tgclientrect.height - 2 + 'px';
+      targid = e.target.parentElement.id;
+    }
+});
+
+navigationbar.addEventListener('mouseleave', (e) => {
+  setTimeout(() => {prepositiontheline()}, 1000)
+});
+
+document.addEventListener('mouseover', function(event) {
+  anchorcoords = event.target.closest('[data-anchor]');
+});
+
+
+window.onresize = function(event) {
+  line.style.top = navigationbar.offsetHeight - 2 + 'px';
+  let currelem = document.querySelector(`#${targid}`);
+  let elempos = currelem.getBoundingClientRect();
+  line.style.left = elempos.left + 'px';
+  anchorcoords.scrollIntoView(top);
+};
